03d88bc5d0d7db61b7db3769e8ef1c77ff3c1ed9
Loki <sergiy@vynnychenko.com>
2019-03-30 15:24:39 +0200

    CollectionView binding

---
+++ Src/Binding/RealmToCollectionView/AsyncCollectionViewDataSource.swift
@@ -0,0 +1,119 @@
+//
+//  CollectionDelegate.swift
+//  TaoGit
+//
+//  Created by Loki on 1/27/19.
+//  Copyright © 2019 Cheka Zuja. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+import RealmSwift
+import Realm
+
+public typealias CollectionItemFactory<E: Object> = (AsyncCollectionViewDataSource<E>, NSCollectionView, IndexPath, E) -> NSCollectionViewItem
+public typealias CollectionItemConfig<E: Object, ItemType: NSCollectionViewItem> = (ItemType, IndexPath, E) -> Void
+
+public class AsyncCollectionViewDataSource<E: Object>: NSObject, NSCollectionViewDelegate, NSCollectionViewDataSource {
+    private var items: AnyRealmCollection<E>?
+    
+    // MARK: - Configuration
+    public var collectionView: NSCollectionView?
+    public var animated = true
+    
+    // MARK: - Init
+    public let itemIdentifier: String
+    public let itemFactory: CollectionItemFactory<E>
+    
+    public weak var delegate: NSCollectionViewDelegate?
+    public weak var dataSource: NSCollectionViewDataSource?
+    
+    public init(itemIdentifier: String, itemFactory: @escaping CollectionItemFactory<E>) {
+        self.itemIdentifier = itemIdentifier
+        self.itemFactory = itemFactory
+    }
+    
+    public init<ItemType>(itemIdentifier: String, itemType: ItemType.Type, itemConfig: @escaping CollectionItemConfig<E, ItemType>) where ItemType: NSCollectionViewItem {
+        self.itemIdentifier = itemIdentifier
+        self.itemFactory = { ds, cv, ip, model in
+            let item = cv.makeItem(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: itemIdentifier), for: ip) as! ItemType
+            itemConfig(item, ip, model)
+            return item
+        }
+    }
+    
+    deinit {
+        AppCore.log(title: "CollectionViewDataSource", msg: "deinit for \(itemIdentifier)")
+    }
+    
+    // MARK: - NSCollectionViewDataSource protocol
+    public func numberOfSections(in collectionView: NSCollectionView) -> Int {
+        return 1
+    }
+    
+    public func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items?.count ?? 0
+    }
+    
+    public func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
+        return itemFactory(self, collectionView, indexPath, items![indexPath.item])
+    }
+    
+    // MARK: - Proxy unimplemented data source and delegate methods
+    open override func responds(to aSelector: Selector!) -> Bool {
+        if AsyncCollectionViewDataSource.instancesRespond(to: aSelector) {
+            return true
+        } else if let delegate = delegate {
+            return delegate.responds(to: aSelector)
+        } else if let dataSource = dataSource {
+            return dataSource.responds(to: aSelector)
+        } else {
+            return false
+        }
+    }
+    
+    open override func forwardingTarget(for aSelector: Selector!) -> Any? {
+        return delegate ?? dataSource
+    }
+    
+    // MARK: - Applying changeset to the collection view
+    private let fromRow = {(row: Int) in return IndexPath(item: row, section: 0)}
+    
+    func applyChanges(items: AnyRealmCollection<E>, changes: RealmChangeset?) {
+        if self.items == nil {
+            self.items = items
+        }
+        
+        guard let collectionView = collectionView else {
+            fatalError("You have to bind a collection view to the data source.")
+        }
+        
+        guard animated else {
+            collectionView.reloadData()
+            return
+        }
+        
+        guard let changes = changes else {
+            collectionView.reloadData()
+            return
+        }
+        
+        let lastItemCount = collectionView.numberOfItems(inSection: 0)
+        guard items.count == lastItemCount + changes.inserted.count - changes.deleted.count else {
+            collectionView.reloadData()
+            return
+        }
+        
+        collectionView.performBatchUpdates({[unowned self] in
+            //TODO: this should be animated, but doesn't seem to be?
+            //NSAnimationContext.current.duration = 1.0
+            
+            collectionView.animator().deleteItems(at: Set(changes.deleted.map(self.fromRow)))
+            collectionView.animator().reloadItems(at: Set(changes.updated.map(self.fromRow)))
+            collectionView.animator().insertItems(at: Set(changes.inserted.map(self.fromRow)))
+            }, completionHandler: nil)
+    }
+    
+}
+
+
---
+++ Src/Binding/RealmToCollectionView/Binding.swift
@@ -0,0 +1,77 @@
+//
+//  Binding.swift
+//  AppCore
+//
+//  Created by Loki on 3/30/19.
+//  Copyright © 2019 Loki. All rights reserved.
+//
+
+import Foundation
+import AsyncNinja
+import RealmSwift
+
+public func bind33<E>(realmQuery: Results<E>, dataSource: AsyncCollectionViewDataSource<E>, view: NSCollectionView) {
+    let sink = createSinkWith(dataSource: dataSource, to: view)
+    
+    changesetChannel(from: realmQuery)
+        .bind(sink)
+    
+}
+
+public func createSinkWith<E>(dataSource: AsyncCollectionViewDataSource<E>, to view: NSCollectionView) -> Sink<(AnyRealmCollection<E>,RealmChangeset?),Void> {
+    
+    return createSinkWith(dataSource: dataSource) { ds, results, changes in
+        if ds.collectionView == nil {
+            ds.collectionView = view
+        }
+        ds.collectionView?.dataSource = ds
+        ds.applyChanges(items: AnyRealmCollection<E>(results), changes: changes)
+    }
+}
+
+func createSinkWith<E>(dataSource: AsyncCollectionViewDataSource<E>, block: @escaping (AsyncCollectionViewDataSource<E>, AnyRealmCollection<E>, RealmChangeset?) -> Void)
+    -> Sink<(AnyRealmCollection<E>,RealmChangeset?),Void> {
+        
+        return Sink(updateExecutor: Executor.main) { sink, event, executor in
+            if case let .update(element) = event {
+                block(dataSource, element.0, element.1)
+            }
+        }
+}
+
+public struct RealmChangeset {
+    /// the indexes in the collection that were deleted
+    public let deleted: [Int]
+    
+    /// the indexes in the collection that were inserted
+    public let inserted: [Int]
+    
+    /// the indexes in the collection that were modified
+    public let updated: [Int]
+    
+    public init(deleted: [Int], inserted: [Int], updated: [Int]) {
+        self.deleted = deleted
+        self.inserted = inserted
+        self.updated = updated
+    }
+}
+
+public func changesetChannel<E: Object>(from collection: Results<E>) -> Channel<(AnyRealmCollection<E>, RealmChangeset?), Void> {
+    
+    return producer() { producer in
+        let notificationToken = collection.toAnyCollection().observe { /*do not forget*/ [weak producer] changeset in
+            
+            switch changeset {
+            case .initial(let value):
+                producer?.update((value, nil))
+            case .update(let value, let deletions, let insertions, let modifications):
+                producer?.update((value, RealmChangeset(deleted: deletions, inserted: insertions, updated: modifications)))
+            case .error(let error):
+                producer?.fail(error)
+            }
+        }
+        
+        producer._asyncNinja_retainUntilFinalization(notificationToken)
+        producer._asyncNinja_notifyFinalization { print("Realm subscription released") }
+    }
+}
