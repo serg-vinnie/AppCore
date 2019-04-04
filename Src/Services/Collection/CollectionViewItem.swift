b856e4e48e9730ffc7414f38e90e14d48fe22656
Loki <sergiy@vynnychenko.com>
2019-04-04 15:48:21 +0300

    ..

---
+++ Src/Services/Collection/CollectionViewItem.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewItem.swift
+//  AppCore
+//
+//  Created by Loki on 4/4/19.
+//  Copyright © 2019 Loki. All rights reserved.
+//
+
+import AppKit
+
+open class CollectionViewItem: NSCollectionViewItem {
+    public var signals : SignalsService?
+    public var key : String = ""
+    
+}
