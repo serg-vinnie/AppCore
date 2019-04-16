af34019b088d117102fb027d764e8eaf42d47311
Loki <sergiy@vynnychenko.com>
2019-04-16 22:08:56 +0300

    CollectionServiceBinder

--- Src/Services/Collection/CollectionServiceBinding.swift
+++ Src/Services/Collection/CollectionServiceBinding.swift
@@ -9,8 +9,49 @@
 import Foundation
 import RealmSwift
 
+public typealias CustomItemConfig<EntityType: CollectionEntity, ItemType: NSCollectionViewItem> = (ItemType, EntityType) -> Void
+
+public class CollectionServiceBinder<EntityType: CollectionEntity, ItemType: NSCollectionViewItem> {
+    private var service         : CollectionService<EntityType>
+    private var customConfig    : CustomItemConfig<EntityType,ItemType>?
+    private var defaultImage    : NSImage?
+    private var delegate   : NSCollectionViewDelegate?
+    
+    public init(service: CollectionService<EntityType>) {
+        self.service = service
+    }
+    
+    deinit {
+        AppCore.log(title: "CustomItemConfig", msg: "deinit")
+    }
+    
+    public func setCustomConfig(_ config: @escaping CustomItemConfig<EntityType,ItemType>) -> CollectionServiceBinder {
+        customConfig = config
+        return self
+    }
+    
+    public func setDefault(image: NSImage?) -> CollectionServiceBinder {
+        defaultImage = image
+        return self
+    }
+    
+    public func set(delegate : NSCollectionViewDelegate?) -> CollectionServiceBinder {
+        self.delegate = delegate
+        return self
+    }
+    
+    public func bindTo(view: NSCollectionView, itemId: String, storyboard: NSStoryboard) {
+        let dataSource = CollectionViewDataSource<EntityType>(
+            itemFactory: collectionItemFactory(storyboard: storyboard, id: itemId, service: service, defaultImage: defaultImage))
+        dataSource.delegate = delegate
+        dataSource.bindWith(realmQuery: service.queryAllItems(), view: view)
+    }
+    
+    
+}
+
 public extension CollectionService {
-    func bindTo(view: NSCollectionView, itemId: String, storyboard: NSStoryboard, defaultImage: String, delegate: NSCollectionViewDelegate) {
+    func bindTo(view: NSCollectionView, itemId: String, storyboard: NSStoryboard, defaultImage: NSImage?, delegate: NSCollectionViewDelegate) {
         let dataSource = CollectionViewDataSource<Entity>(
             itemFactory: collectionItemFactory(storyboard: storyboard, id: itemId, service: self, defaultImage: defaultImage))
         
@@ -19,7 +60,7 @@ public extension CollectionService {
     }
 }
 
-public func collectionItemFactory<E: CollectionEntity>(storyboard: NSStoryboard, id: String, service: CollectionService<E>, defaultImage: String) -> CollectionItemFactory<E> {
+public func collectionItemFactory<E: CollectionEntity>(storyboard: NSStoryboard, id: String, service: CollectionService<E>, defaultImage: NSImage?) -> CollectionItemFactory<E> {
     return { dataSource, view, indexPath, realmItem in
         let item = storyboard.viewController(id: id) as! CollectionViewItem
         let realmItem = realmItem as CollectionEntityProtocol
@@ -31,7 +72,7 @@ public func collectionItemFactory<E: CollectionEntity>(storyboard: NSStoryboard,
         if realmItem.iconPath.count > 0 {
             item.image = NSImage(byReferencingFile: service.thumbnails.url.appendingPathComponent(realmItem.iconPath).path)
         } else {
-            item.image = NSImage(named: defaultImage)
+            item.image = defaultImage
         }
         
         return item
