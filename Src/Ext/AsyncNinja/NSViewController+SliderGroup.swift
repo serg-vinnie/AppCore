c3b4b2f8683a27fde1d1e33b048352912262dbd8
Loki <sergiy@vynnychenko.com>
2019-06-22 12:50:21 +0300

    initGroupFor(silder: titleChannel

--- Src/Ext/AsyncNinja/NSViewController+SliderGroup.swift
+++ Src/Ext/AsyncNinja/NSViewController+SliderGroup.swift
@@ -32,4 +32,27 @@ public extension ExecutionContext where Self: NSViewController {
             .delayedUpdate(timeout: 1)
             .onUpdate(context: self) { _,_ in valueText.isHidden = autoHide ? true : false }
     }
+    
+    func initGroupFor(slider: NSSlider, valueText: NSTextField,  config: ConfigR<Int64>, title: Channel<String?,Void>, autoHide: Bool = true) {
+        
+        let sliderValue = slider.rp.integerValue
+            .skip(first: 1, last: 0)
+            .filter() { $0 != nil }
+            .map { Int64($0!) }
+        
+        sliderValue
+            .onUpdate(context: self) { _, value in
+                config.value = value
+                valueText.isHidden = false
+        }
+        
+        self.combineLatest(sliderValue, title)
+            .map { "\($0.0) \($0.1!)" }
+            .bind(valueText.rp.stringValue)
+        
+        sliderValue
+            .debounce(interval: 1)
+            .delayedUpdate(timeout: 1)
+            .onUpdate(context: self) { _,_ in valueText.isHidden = autoHide ? true : false }
+    }
 }
