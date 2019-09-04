9291a2efba1e55b16a39dce159b2188988302249
Loki <sergiy@vynnychenko.com>
2019-09-04 15:37:40 +0300

    macAddress() and SN

--- Src/Helpers/Environment.swift
+++ Src/Helpers/Environment.swift
@@ -40,3 +40,19 @@ public func deviceName() -> String {
     
     #endif
 }
+
+public func serialNumber() -> String {
+    // Get the platform expert
+    let platformExpert: io_service_t = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("IOPlatformExpertDevice"));
+    
+    // Get the serial number as a CFString ( actually as Unmanaged<AnyObject>! )
+    let serialNumberAsCFString = IORegistryEntryCreateCFProperty(platformExpert, kIOPlatformSerialNumberKey as CFString, kCFAllocatorDefault, 0);
+    
+    // Release the platform expert (we're responsible)
+    IOObjectRelease(platformExpert);
+    
+    // Take the unretained value of the unmanaged-any-object
+    // (so we're not responsible for releasing it)
+    // and pass it back as a String or, if it fails, an empty string
+    return (serialNumberAsCFString?.takeUnretainedValue() as? String) ?? ""
+}
