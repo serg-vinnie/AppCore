303a3edd2ba2b6f95348fb661471d2cf8b4acd25
Loki <sergiy@vynnychenko.com>
2019-10-20 22:20:22 +0300

    AppCoreController for SwiftUI

---
+++ Src/AppCoreController.swift
@@ -0,0 +1,23 @@
+//
+//  AppCoreController.swift
+//  AppCore
+//
+//  Created by Loki on 20.10.2019.
+//  Copyright © 2019 Loki. All rights reserved.
+//
+
+import SwiftUI
+
+@available(OSX 10.15, *)
+open class AppCoreController<MyView,MyViewModel> : NSHostingController<MyView> where MyView : View  {
+	public var      viewModel       	: MyViewModel! { didSet { viewModelDidSet() } }
+	open var        resolveViewModel   	: Bool { return true }
+	
+	@objc required dynamic public init?(coder: NSCoder) {
+		super.init(coder: coder, rootView: AppCore.container.resolve(MyView.self)!)
+	}
+	
+	open func viewModelDidSet() {
+        
+    }
+}
